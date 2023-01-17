516f2f00044f9423200971aff0be73f26c7a2f06
Halcyon <ctsev1987@gmail.com>
2023-01-17 19:20:27 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,37 @@ import restarticon from './assets/restarticon.png'
 const WORDS = wordList.slice(10000)
 
 function App() {
-    const [time, setTime] = useState(30000)
-    const [words, setWords] = useState(function(){
+    const [words, setWords] = useState(function(){ //State for words that should be typed
         let outp = []
         for (let i = 0; i < 9; i++) {
             outp[i] = {
-                body: i < 4 ? 'o' : WORDS[Math.ceil(Math.random() * 200000)],
+                body: i < 4 ? 'o' : WORDS[Math.ceil(Math.random() * 160000)],
                 status: i < 4 ? 'filler' : 'queued'
             }
         }
         return outp
     })
-    const [input, setInput] = useState('')
-    const [correct, setCorrect] = useState(true)
-    const [wordStorage,setWordStorage] = useState([])
-    const [started, setStarted] = useState(false)
-    const [timerActive,setTimerActive] = useState(false)
+    const [input, setInput] = useState('') //State for controlled input
+    const [correct, setCorrect] = useState(true) //State that determines wheter input is matching given word or not
+    const [wordStorage,setWordStorage] = useState([]) //State that stores words to precisely calculate WPM and CPM, along with potential future uses
+    const [started, setStarted] = useState(false) //State that determines whether the user has started typing or not
+    const [definedTime, setDefinedTime] = useState(30000) //State that determines what the defined time is
+    const [time, setTime] = useState(definedTime) //State that keeps track of time rundown
+
+    useEffect(() => { //At the start of the program, makes a setInterval that deducts 100 from definedTime every 100ms. This will not fire until the race starts due to [1]
+        setInterval(() => {
+            setTime(prev => prev - 100)
+        }, 100)
+    }, [])
+    
     useEffect(() => {
-        if (started) {
-            let timer = setInterval(() => {
-                if (timerActive) {setTime(prev => {
-                    return prev - 100
-                })}
-            }, 100)
+        if (time <= 0) {
+            document.getElementById('rst').style.display = 'block' // Display reset button when finished
         }
-    }, [started])
+        if (!started) {
+            setTime(definedTime) // [1] - If game has not started, set time back to defined time as a preventative measure against setInterval
+        }
+    }, [time])
 
     const checkWord = (val, final = false) => { //final arg checks if the entire word matches, not just the input part that is used for displaying
         let correct = true
@@ -48,23 +54,23 @@ function App() {
         return correct
     }
 
-    function handleInput(event) {
+    function handleMainInput(event) { //Used for controlled input
         const value = event.target.value
-        setTimerActive(true)
+        if(event.nativeEvent.inputType === "insertLineBreak") return; //if user pressed enter dont insert new line
         setStarted(true)
-        if (value[value.length - 1] == ' ') {
-            if (input === '') {return}
-            if (time <= 0) {return}
-            setInput('')
+        if (value[value.length - 1] == ' ') { //Checks if user has pressed space (finished input)
+            if (input === '') {return} //If input is empty deny finish
+            if (time <= 0) {return} //If time is up deny finish
+            setInput('') 
             let checker = checkWord(value, true)
             setWords(prev => {
                 let outp = [...prev]
                 outp.shift()
                 outp[3].status = checker ? 'correct' : 'incorrect'
                 outp[3].body = value.slice(0, -1)
-                setWordStorage(prev => [...prev, outp[3]])
+                setWordStorage(prev => [...prev, outp[3]]) //Word storage gets a new element equal to the object of the input body and whether its correct or not
                 outp[8] = {
-                    body: WORDS[Math.ceil(Math.random() * 200000)],
+                    body: WORDS[Math.ceil(Math.random() * 160000)],
                     status: 'queued'
                 } 
                 return outp
@@ -80,7 +86,8 @@ function App() {
     function restart() { //function for restarting the game obvviously
         setWordStorage([])
         setInput('')
-        setTime(30000)
+        setTime(definedTime)
+        setStarted(false)
         setCorrect(true)
         setWords(prev => {
             let outp = []
@@ -92,44 +99,29 @@ function App() {
             }
             return outp
         })
-        setTimerActive(false)
         document.getElementById('rst').style.display = 'none'
         document.getElementById('minput').focus()
     }
 
-    useEffect(() => {
-        if (time <= 0) {
-            document.getElementById('rst').style.display = 'block'
-            setTimerActive(false)
-        }
-    }, [time])
+
     return (
         <React.Fragment>
-            <Header time={time} words={wordStorage}/>
+            <Header time={time} words={wordStorage} started={started} setDef={setDefinedTime} definedTime={definedTime}/>
             <div className='--main-wrapper'>
                 <Word size='0.8868' word={words[0]}/>
                 <Word size='1.2157' word={words[1]}/>
                 <Word size='2.0447' word={words[2]}/>
                 <Word size='3.4239' word={words[3]}/>
                 <textarea className='--main-word' rows='1' cols={words[4].body.length} value={words[4].body}></textarea>
-                <textarea className='--main-input' onInput={handleInput} rows='1' cols={words[4].body.length > input.length ? words[4].body.length : input.length} 
+                <textarea className='--main-input' onInput={handleMainInput} rows='1' cols={words[4].body.length > input.length ? words[4].body.length : input.length} 
                 name='input' value={input} style={{color:correct ? 'rgb(54, 54, 54)' : 'red'}} spellCheck='false' maxLength={20} id='minput'></textarea>
                 <Word size='3.4239' word={words[5]}/>
                 <Word size='2.0447' word={words[6]}/>
                 <Word size='1.2157' word={words[7]}/>
                 <Word size='0.8868' word={words[8]}/>
-                <button id='rst' className='--restart-btn'><img className='--restart-img' src={restarticon} onClick={restart}/></button>
+                <button id='rst' className='--restart-btn' onClick={restart}><img className='--restart-img' src={restarticon}/></button>
                 <ul className="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
                 </ul>
             </div>
         </React.Fragment>
--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function Header(props) {
     let [accuracy, setAccuracy] = React.useState(100)
     let [speed, setSpeed] = React.useState({wpm: 0, cpm: 0})
-    let [dispTime, setDispTime] = React.useState(30)
+    let [dispTime, setDispTime] = React.useState(props.time / 1000)
+
     const calcCPM = () => {
         let sum = 0
         for (let word of props.words) {sum += word.body.length}
@@ -15,13 +16,17 @@ function Header(props) {
             if (item.status === 'correct') {++corSum}
         }
         setAccuracy(props.words.length === 0 ? 100 : Math.ceil(((corSum * 100) / props.words.length) * 100) / 100)
-        setSpeed({wpm: dispTime < 30 ? Math.ceil(((props.words.length / (30 - dispTime)) * 60 ) * 100) / 100 : 0, 
+        setSpeed({wpm: dispTime < 30 ? Math.ceil(((props.words.length / (30 - dispTime)) * 80 ) * 100) / 100 : 0, //I put 80 instead of 60 for some leniency towards the user, since most words are BS 
                 cpm: calcCPM()}) 
     }, [dispTime])
 
     React.useEffect(() => { //every time controlled time changes calculate display time
-        setDispTime(props.time <= 0 ? 0 : props.time / 1000)
+        setDispTime(prev => {
+            if (!props.started) {return prev}
+            if (props.time <= 0) {return 0} else {return props.time / 1000}
+        })
     }, [props.time])
+
 
     return (
         <div className='--header'>
@@ -29,18 +34,26 @@ function Header(props) {
                 <h1 className='--header-logo'>Typerate</h1>
                 <div className='--header-blinker'></div>
             </div>
-            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around', width:'50%'}}>
-                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '24%'}}>
+            <div className='--header-container' style={{width: '50%'}}>
+                <div className='--header-container items'>
                     <h1 className='--header-info'>{accuracy}%</h1>
                     <h6 className='--header-info-sub'>Accuracy</h6>
                 </div>
-                <div style={{width:'24%', textAlign: 'center'}}>
-                    <h2 className='--header-info' style={{margin:0, fontSize: '1.2157vw'}}>{speed.wpm}wpm</h2>
-                    <h2 className='--header-info' style={{margin:0, fontSize: '1.2157vw'}}>{speed.cpm}cpm</h2>
-                    <h6 className='--header-info-sub' style={{left:'72.2%'}}>Speed</h6>
+                <div className='--header-container items'>
+                    <div style={{width:'90%', textAlign: 'center'}}>
+                        <div style={{display: 'flex', justifyContent:'space-between', alignItems: 'center'}}>
+                            <h2 className='--header-info cwpm' style={{margin:0, fontSize: '1.5157vw'}}>{speed.wpm}</h2>
+                            <h2 className='--header-info label'>wpm</h2>
+                        </div>
+                        <div style={{display: 'flex', justifyContent:'space-between', alignItems: 'center'}}>
+                            <h2 className='--header-info cwpm' style={{margin:0, fontSize: '1.5157vw'}}>{speed.cpm}</h2>
+                            <h2 className='--header-info label'>cpm</h2>
+                        </div>
+                        <h6 className='--header-info-sub' style={{left:'66.2%'}}>Speed</h6>
+                    </div>
                 </div>
-                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width:'24%'}}>
-                    <h1 className='--header-info'>{dispTime}s</h1>
+                <div className='--header-container items'>
+                    <input className='--header-time' spellCheck={false} value={`${dispTime}s`}></input>
                     <h6 className='--header-info-sub'>Time</h6>
                 </div>
             </div>
@@ -48,4 +61,4 @@ function Header(props) {
     )
 }
 
-export default React.memo(Header)
+export default Header
